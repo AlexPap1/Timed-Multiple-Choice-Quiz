5b3b59e861acc1337ea84124506ebd3163b13624
AlexPap <arpappagallo@gmail.com>
2022-04-01 13:29:10 -0400

    I'm so lost

--- Assets/script.js
+++ Assets/script.js
@@ -1,77 +1,17 @@
-//questions
-const questions = [
-    {
-        question: "Yes or No?",
-        choices: ["a. yes", "b. no", "c. no", "d. no"],
-        answer: "a. yes"
-    },
-    {
-        question: "Yes or No?",
-        choices: ["a. yes", "b. no", "c. no", "d. no"],
-        answer: "a. yes"
-    },
-    {
-        question: "Yes or No?",
-        choices: ["a. yes", "b. no", "c. no", "d. no"],
-        answer: "a. yes"
-    },
-    {
-        question: "Yes or No?",
-        choices: ["a. yes", "b. no", "c. no", "d. no"],
-        answer: "a. yes"
-    },
-    {
-        question: "Yes or No?",
-        choices: ["a. yes", "b. no", "c. no", "d. no"],
-        answer: "a. yes"
-    },
-    {
-        question: "Yes or No?",
-        choices: ["a. yes", "b. no", "c. no", "d. no"],
-        answer: "a. yes"
-    },
-    {
-        question: "Yes or No?",
-        choices: ["a. yes", "b. no", "c. no", "d. no"],
-        answer: "a. yes"
-    },
-    {
-        question: "Yes or No?",
-        choices: ["a. yes", "b. no", "c. no", "d. no"],
-        answer: "a. yes"
-    },
-    {
-        question: "Yes or No?",
-        choices: ["a. yes", "b. no", "c. no", "d. no"],
-        answer: "a. yes"
-    },
-    {
-        question: "Yes or No?",
-        choices: ["a. yes", "b. no", "c. no", "d. no"],
-        answer: "a. yes"
-    }
-]; 
-
-//variables defined
-
 var timerEl = document.getElementById('countdown');
-var questionOrder = 0;
+const questionContainer = document.getElementById('questionContainer');
 var startButton = document.getElementById('start-btn');
 var welcome = document.getElementById('welcome');
-var questionName = document.getElementById('questionName');
-var answerA = document.getElementById("answerA");
-var answerB = document.getElementById("answerB");
-var answerC = document.getElementById("answerC");
-var answerD = document.getElementById("answerD");
+var questionOrder = 0;
+const questionEl = document.getElementById('questionName');
 
-
-// Timer set and starting quiz
-
+//Start Button and Timer Set
 startButton.addEventListener('click', 
 function countdown() {
+    startButton.style.visibility = "hidden";
+    welcome.style.visibility = "hidden";
     questionOrder = 0;
     var timeLeft = 60;
-
     var timeInterval = setInterval(function () {
         if (timeLeft > 0) {
             timerEl.textContent = "Timer: " + timeLeft;
@@ -81,12 +21,251 @@ function countdown() {
             clearInterval(timeInterval);
     }
     }, 1000);
-    startButton.style.visibility = "hidden";
-    welcome.style.visibility = "hidden";
     startQuiz();
+    questionContainer.classList.remove('hide')
 }
 );
 
+function startQuiz() {
+    showQuestion(questions[questionOrder])
+}
+
+function showQuestion(question) {
+    questionEl.innerText = question.question
+}
+
+//questions
+const questions = [
+    {
+        question: "Yes or No?",
+        choices: [
+            {text: 'a. yes', correct: true },
+            {text: 'b. no', correct: false },
+            {text: 'c. no', correct: false },
+            {text: 'd. no', correct: false },
+        ]
+    },
+    {
+        question: "Yes or No?",
+        choices: [
+            {text: 'a. yes', correct: true },
+            {text: 'b. no', correct: false },
+            {text: 'c. no', correct: false },
+            {text: 'd. no', correct: false },
+        ]
+    },
+    {
+        question: "Yes or No?",
+        choices: [
+            {text: 'a. yes', correct: true },
+            {text: 'b. no', correct: false },
+            {text: 'c. no', correct: false },
+            {text: 'd. no', correct: false },
+        ]
+    },
+    {
+        question: "Yes or No?",
+        choices: [
+            {text: 'a. yes', correct: true },
+            {text: 'b. no', correct: false },
+            {text: 'c. no', correct: false },
+            {text: 'd. no', correct: false },
+        ]
+    },
+    {
+        question: "Yes or No?",
+        choices: [
+            {text: 'a. yes', correct: true },
+            {text: 'b. no', correct: false },
+            {text: 'c. no', correct: false },
+            {text: 'd. no', correct: false },
+        ]
+    },
+    {
+        question: "Yes or No?",
+        choices: [
+            {text: 'a. yes', correct: true },
+            {text: 'b. no', correct: false },
+            {text: 'c. no', correct: false },
+            {text: 'd. no', correct: false },
+        ]
+    },
+    {
+        question: "Yes or No?",
+        choices: [
+            {text: 'a. yes', correct: true },
+            {text: 'b. no', correct: false },
+            {text: 'c. no', correct: false },
+            {text: 'd. no', correct: false },
+        ]
+    },
+    {
+        question: "Yes or No?",
+        choices: [
+            {text: 'a. yes', correct: true },
+            {text: 'b. no', correct: false },
+            {text: 'c. no', correct: false },
+            {text: 'd. no', correct: false },
+        ]
+    },
+    {
+        question: "Yes or No?",
+        choices: [
+            {text: 'a. yes', correct: true },
+            {text: 'b. no', correct: false },
+            {text: 'c. no', correct: false },
+            {text: 'd. no', correct: false },
+        ]
+    },
+    {
+        question: "Yes or No?",
+        choices: [
+            {text: 'a. yes', correct: true },
+            {text: 'b. no', correct: false },
+            {text: 'c. no', correct: false },
+            {text: 'd. no', correct: false },
+        ]
+    }
+];
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+//variables defined
+
+var score = 0;
+var questionName = document.getElementById('questionName');
+var answerA = document.getElementById("answerA");
+var answerB = document.getElementById("answerB");
+var answerC = document.getElementById("answerC");
+var answerD = document.getElementById("answerD");
+
+// Timer set and starting quiz
+
+var timerEl = document.getElementById('countdown');
+const questionContainerElement = document.getElementById('questionContainer');
+var startButton = document.getElementById('start-btn');
+var welcome = document.getElementById('welcome');
+
+startButton.addEventListener('click', 
+function countdown() {
+    startButton.style.visibility = "hidden";
+    welcome.style.visibility = "hidden";
+    questionOrder = 0;
+    var timeLeft = 60;
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 0) {
+            timerEl.textContent = "Timer: " + timeLeft;
+            timeLeft--;
+        } else {
+            timerEl.textContent = 'Timer: 00';
+            clearInterval(timeInterval);
+    }
+    }, 1000);
+    questionContainerElement.classList.remove('hide')
+    startQuiz();
+}
+);
+/*
 //start quiz function
 
 function startQuiz() {
@@ -94,14 +273,17 @@ function startQuiz() {
 }
 
 function nextQuestion() {
-    questionName.textContent = questions[questionOrder].question;
-    answerA.textContent = questions[questionOrder].choices[0];
-    answerB.textContent = questions[questionOrder].choices[1];
-    answerC.textContent = questions[questionOrder].choices[2];
-    answerD.textContent = questions[questionOrder].choices[3];
+    for (var i = 0; i < questions.length; i++) {
+        questionName.textContent = questions[questionOrder].question;
+        answerA.textContent = questions[questionOrder].choices[0];
+        answerB.textContent = questions[questionOrder].choices[1];
+        answerC.textContent = questions[questionOrder].choices[2];
+     answerD.textContent = questions[questionOrder].choices[3];
+    }
 }
-
+*/
 //check answer function
+// tell user if right or wrong
 //game over (timer over)
 //game over (questions answered)
 //high score function
